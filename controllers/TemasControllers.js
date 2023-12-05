8a73cda22972be11a12bdd852e4f81e9a5047748
Andre R <andreruizvisfocri@gmail.com>
2023-12-05 11:39:48 -0600

    actualizacion

--- controllers/TemasControllers.js
+++ controllers/TemasControllers.js
@@ -15,6 +15,51 @@ class TemasController {
         }
         res.send(data[0]);
     }
+    static async indexPost(req, res) {
+        try {
+            const newData = req.body;
+
+            const insertedId = await TemasModel.insertar(newData);
+
+            res.status(201)
+                .header('Location', `/temas/${insertedId}`)
+                .send({status: 201, message: 'Created'});
+        } catch (error) {
+            res.status(400).send({ errno: 400, error: 'Bad Request' });
+        }
+    }
+    static async itemPut(req, res) {
+        try {
+            const id = req.params.id;
+            const updatedData = req.body;
+
+            const result = await TemasModel.reemplazar(id, updatedData);
+
+            if (result === 0) {
+                res.status(404).send({ errno: 404, error: 'Not found' });
+            } else {
+                res.send({ message: 'Updated successfully'});
+            }
+        } catch (error) {
+            res.status(400).send({ errno: 400, error: 'Bad Request'});
+        }
+    }
+    static async itemPatch(req, res) {
+        try {
+            const id = req.params.id;
+            const updatedFields = req.body;
+
+            const result = await TemasModel.actualizar(id, updatedFields);
+
+            if (result === 0) {
+                res.status(404).send({ errno: 404, error: 'Not found' });
+            } else {
+                res.send({ message: 'Successfull partial update'});
+            }
+        } catch (error) {
+            res.status(400).send({ errno: 400, error: 'Bad Request' });
+        }
+    }
 }
 
 module.exports = TemasController;
