a5853ed8096e98ad7609bb094155011ef9283c43
Andre R <andreruizvisfocri@gmail.com>
2023-12-08 12:14:12 -0600

    modificacionPasswords

--- dbconnection.js
+++ dbconnection.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const knex = require("knex");
 
 async function connectMysql() {
-    const mysqlPassword = await fs.promises.readFile('/run/secrets/mysql_passwords');
+    const mysqlPassword = await fs.promises.readFile('/run/secrets/mysql_password');
     const db = knex({
         client: 'mysql2',
         connection: {
