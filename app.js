0fc293f7b9b9fe59c768002996da66032e707e9e
Andre R <andreruizvisfocri@gmail.com>
2023-12-08 12:56:28 -0600

    añadir post patch y put

--- app.js
+++ app.js
@@ -23,6 +23,24 @@ app.get('/brechas_seguridad', brechas_seguridadController.indexGet);
 app.get('/informacion_aplicacion', informacion_aplicacionController.indexGet);
 app.get('/usuarios_brechas', usuarios_brechasController.indexGet);
 
+app.post('/usuarios', usuariosController.indexPost);
+app.patch('/usuarios/:id([0-9]+)', usuariosController.itemPatch);
+app.put('/usuarios/:id([0-9]+)', usuariosController.itemPut);
+
+app.post('/brechas_seguridad', brechas_seguridadController.indexPost);
+app.patch('/brechas_seguridad/:id([0-9]+)', brechas_seguridadController.itemPatch);
+app.put('/brechas_seguridad/:id([0-9]+)', brechas_seguridadController.itemPut);
+
+app.post('/informacion_aplicacion', informacion_aplicacionController.indexPost);
+app.patch('/informacion_aplicacion/:id([0-9]+)', informacion_aplicacionController.itemPatch);
+app.put('/informacion_aplicacion/:id([0-9]+)', informacion_aplicacionController.itemPut);
+
+app.post('/usuarios_brechas', usuarios_brechasController.indexPost);
+app.patch('/usuarios_brechas/:id([0-9]+)', usuarios_brechasController.itemPatch);
+app.put('/usuarios_brechas/:id([0-9]+)', usuarios_brechasController.itemPut);
+
+
+
 app.listen(puerto, function(){
     console.log("Servidor en espera http://localhost");
 });
