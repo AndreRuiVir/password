3122a38049cd746a778c88457a3755b2d1dacc20
Andre R <andreruizvisfocri@gmail.com>
2023-12-08 19:07:10 -0600

    correcion mayusculas en informacion_aplicacion

--- models/informacion_aplicacion.js
+++ models/informacion_aplicacion.js
@@ -3,31 +3,31 @@ class Informacion_AplicacionModel
 {
     static async consultar() {
         let db = await connectMysql();
-        let query = db('Informacion_Aplicacion');
+        let query = db('informacion_aplicacion');
         return await query;
     }
 
     static async consultarPorId(id) {
         let db = await connectMysql();
-        return await db('Informacion_Aplicacion').where('id_informacion', id);
+        return await db('informacion_aplicacion').where('id_informacion', id);
     }
 
     static async insertar(datos) {
         let db = await connectMysql();
-        const result = await db('Informacion_Aplicacion').insert(datos).returning('id_informacion');
+        const result = await db('informacion_aplicacion').insert(datos).returning('id_informacion');
         return result[0];
     }
 
     static async actualizar(id, campos) {
         let db = await connectMysql();
-        return await db('Informacion_Aplicacion').where('id_informacion', id).update(campos);
+        return await db('informacion_aplicacion').where('id_informacion', id).update(campos);
     }
 
     static async reemplazar(id, newData) {
         let db = await connectMysql();
         newData['id_informacion'] = id;
         await db.where('id_informacion', id).del();
-        await db.insert(newData).into('Informacion_Aplicacion');
+        await db.insert(newData).into('informacion_aplicacion');
         return id;
     }
 }
